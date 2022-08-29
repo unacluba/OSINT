8357e469b38706320180f637c4c2b6aeb54ae9e6
unacluba <104894720+unacluba@users.noreply.github.com>
2022-08-29 21:06:02 +0200

    Add files via upload

---
+++ huntbreacheddata.sh
@@ -0,0 +1,16 @@
+from subprocess import _TXT
+
+# Download breached password list magnet:
+# magnet:?xt=urn:btih:7ffbcd8cee06aba2ce6561688cf68ce2addca0a3&dn=BreachCompilation&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fglotorrents.pw%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337
+
+# Install and run the tooling:
+
+$domain = 'etc'
+
+git clone https://github.com/hmaverickadams/breach-parse
+cd breach-parse
+sudo ./install.sh
+
+theHarvester -d $domain -b google -1 500
+./breach-parse.sh $domain $domain.txt
+h8mail -t bla@bla.com -bc "/opt/breach-parse/BreachCompilation/" -sk
